2cb1932ad99510fb0b50cb014a7cc3a032356cb6
apstndb <gamaguchi@gmail.com>
2020-06-10 00:06:58 +0900

    Add TestGetNodeTitleAndGetAllMetadataString

--- go.mod
+++ go.mod
@@ -14,4 +14,5 @@ require (
 	google.golang.org/api v0.21.0
 	google.golang.org/genproto v0.0.0-20200416231807-8751e049a2a0
 	google.golang.org/grpc v1.28.1
+	google.golang.org/protobuf v1.21.0
 )
---
+++ query_plan_test.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"testing"
+
+	"google.golang.org/genproto/googleapis/spanner/v1"
+	"google.golang.org/protobuf/types/known/structpb"
+)
+
+func TestGetNodeTitleAndGetAllMetadataString(t *testing.T) {
+	for _, test := range []struct{
+		node          *Node
+		wantNodeTitle string
+		wantMetadataString string
+	}{
+		{&Node{PlanNode: &spanner.PlanNode{
+			DisplayName: "Distributed Union",
+			Metadata:    &structpb.Struct{
+				Fields: map[string]*structpb.Value{
+					"call_type": {Kind: &structpb.Value_StringValue{StringValue: "Local"}},
+					"subquery_cluster_node": {Kind: &structpb.Value_StringValue{StringValue: "4"}},
+				},
+			},
+		}}, "Local Distributed Union", "",
+		},
+		{&Node{PlanNode: &spanner.PlanNode{
+			DisplayName: "Scan",
+			Metadata:    &structpb.Struct{
+				Fields: map[string]*structpb.Value{
+					"scan_type": {Kind: &structpb.Value_StringValue{StringValue: "IndexScan"}},
+					"scan_target": {Kind: &structpb.Value_StringValue{StringValue: "SongsBySongName"}},
+					"Full scan": {Kind: &structpb.Value_StringValue{StringValue: "true"}},
+				},
+			},
+		}}, "Index Scan", "(Index: SongsBySongName, Full scan: true)"},
+		{&Node{PlanNode: &spanner.PlanNode{
+			DisplayName: "Scan",
+			Metadata:    &structpb.Struct{
+				Fields: map[string]*structpb.Value{
+					"scan_type": {Kind: &structpb.Value_StringValue{StringValue: "TableScan"}},
+					"scan_target": {Kind: &structpb.Value_StringValue{StringValue: "Songs"}},
+				},
+			},
+		}}, "Table Scan", "(Table: Songs)"},
+		{&Node{PlanNode: &spanner.PlanNode{
+			DisplayName: "Scan",
+			Metadata:    &structpb.Struct{
+				Fields: map[string]*structpb.Value{
+					"scan_type": {Kind: &structpb.Value_StringValue{StringValue: "BatchScan"}},
+					"scan_target": {Kind: &structpb.Value_StringValue{StringValue: "$v2"}},
+				},
+			},
+		}}, "Batch Scan", "(Batch: $v2)"},
+		{&Node{PlanNode: &spanner.PlanNode{
+			DisplayName: "Sort Limit",
+			Metadata:    &structpb.Struct{
+				Fields: map[string]*structpb.Value{
+					"call_type": {Kind: &structpb.Value_StringValue{StringValue: "Local"}},
+				},
+			},
+		}}, "Local Sort Limit", ""},
+		{&Node{PlanNode: &spanner.PlanNode{
+			DisplayName: "Sort Limit",
+			Metadata:    &structpb.Struct{
+				Fields: map[string]*structpb.Value{
+					"call_type": {Kind: &structpb.Value_StringValue{StringValue: "Global"}},
+				},
+			},
+		}}, "Global Sort Limit", ""},
+		{&Node{PlanNode: &spanner.PlanNode{
+			DisplayName: "Aggregate",
+			Metadata:    &structpb.Struct{
+				Fields: map[string]*structpb.Value{
+					"iterator_type": {Kind: &structpb.Value_StringValue{StringValue: "Stream"}},
+				},
+			},
+		}}, "Stream Aggregate", ""},
+	} {
+		if got := getNodeTitle(test.node); got != test.wantNodeTitle {
+			t.Errorf("getNodeTitle(%#v) = %q but want %q", test.node, got, test.wantNodeTitle)
+		}
+		if got := getAllMetadataString(test.node); got != test.wantMetadataString {
+			t.Errorf("getAllMetadataString(%#v) = %q but want %q", test.node, got, test.wantMetadataString)
+		}
+	}
+}
