9bec01e3bf73c530a59eb740ca867ade4a538615
Yuki Furuyama <furuyama@google.com>
2020-12-20 19:26:24 +0900

    Defer transaction heartbeat to the time when user executes SELECT/DML.

--- session.go
+++ session.go
@@ -19,18 +19,16 @@ package main
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 
 	"cloud.google.com/go/spanner"
-	adminapi "cloud.google.com/go/spanner/admin/database/apiv1"
 	"google.golang.org/api/option"
 	"google.golang.org/grpc/codes"
-)
 
-type txnFinishResult struct {
-	CommitTimestamp time.Time
-	Err             error
-}
+	adminapi "cloud.google.com/go/spanner/admin/database/apiv1"
+	pb "google.golang.org/genproto/googleapis/spanner/v1"
+)
 
 var clientConfig = spanner.ClientConfig{
 	SessionPoolConfig: spanner.SessionPoolConfig{
@@ -54,7 +52,8 @@ type Session struct {
 
 	// for read-write transaction
 	rwTxn         *spanner.ReadWriteStmtBasedTransaction
-	heartbeatChan chan bool
+	rwTxnMutex    sync.Mutex
+	sendHeartbeat bool
 
 	// for read-only transaction
 	roTxn *spanner.ReadOnlyTransaction
@@ -74,7 +73,7 @@ func NewSession(ctx context.Context, projectId string, instanceId string, databa
 		return nil, err
 	}
 
-	return &Session{
+	session := &Session{
 		ctx:         ctx,
 		projectId:   projectId,
 		instanceId:  instanceId,
@@ -82,32 +81,131 @@ func NewSession(ctx context.Context, projectId string, instanceId string, databa
 		client:      client,
 		clientOpts:  opts,
 		adminClient: adminClient,
-	}, nil
+	}
+	go session.startHeartbeat()
+
+	return session, nil
 }
 
-func (s *Session) StartRwTxn(txn *spanner.ReadWriteStmtBasedTransaction) {
+func (s *Session) InReadWriteTransaction() bool {
+	return s.rwTxn != nil
+}
+
+func (s *Session) InReadOnlyTransaction() bool {
+	return s.roTxn != nil
+}
+
+func (s *Session) BeginReadWriteTransaction() error {
+	txn, err := spanner.NewReadWriteStmtBasedTransaction(s.ctx, s.client)
+	if err != nil {
+		return err
+	}
 	s.rwTxn = txn
+	return nil
 }
 
-func (s *Session) FinishRwTxn() {
+func (s *Session) CommitReadWriteTransaction() (time.Time, error) {
+	if s.rwTxn == nil {
+		// TODO: error
+	}
+
+	s.rwTxnMutex.Lock()
+	defer s.rwTxnMutex.Unlock()
+
+	ts, err := s.rwTxn.Commit(s.ctx)
 	s.rwTxn = nil
+	s.sendHeartbeat = false
+	return ts, err
 }
 
-func (s *Session) StartRoTxn(txn *spanner.ReadOnlyTransaction) {
+func (s *Session) RollbackReadWriteTransaction() {
+	if s.rwTxn == nil {
+		// TODO: error
+	}
+
+	s.rwTxnMutex.Lock()
+	defer s.rwTxnMutex.Unlock()
+
+	s.rwTxn.Rollback(s.ctx)
+	s.rwTxn = nil
+	s.sendHeartbeat = false
+}
+
+func (s *Session) BeginReadOnlyTransaction(typ timestampBoundType, staleness time.Duration, timestamp time.Time) (time.Time, error) {
+	txn := s.client.ReadOnlyTransaction()
+	switch typ {
+	case strong:
+		txn = txn.WithTimestampBound(spanner.StrongRead())
+	case exactStaleness:
+		txn = txn.WithTimestampBound(spanner.ExactStaleness(staleness))
+	case readTimestamp:
+		txn = txn.WithTimestampBound(spanner.ReadTimestamp(timestamp))
+	}
+
+	// Because google-cloud-go/spanner defers calling BeginTransaction RPC until an actual query is run,
+	// we explicitly run a "SELECT 1" query so that we can determine the timestamp of read-only transaction.
+	if err := txn.Query(s.ctx, spanner.NewStatement("SELECT 1")).Do(func(r *spanner.Row) error {
+		return nil
+	}); err != nil {
+		return time.Time{}, err
+	}
+
 	s.roTxn = txn
+
+	return txn.Timestamp()
 }
 
-func (s *Session) FinishRoTxn() {
+func (s *Session) CloseReadOnlyTransaction() {
+	if s.roTxn == nil {
+		// TODO: error
+	}
 	s.roTxn.Close()
 	s.roTxn = nil
 }
 
-func (s *Session) InRwTxn() bool {
-	return s.rwTxn != nil
+func (s *Session) RunQueryWithStats(stmt spanner.Statement) (*spanner.RowIterator, *spanner.ReadOnlyTransaction) {
+	if s.rwTxn != nil {
+		defer func() { s.sendHeartbeat = true }()
+		return s.rwTxn.QueryWithStats(s.ctx, stmt), nil
+	} else if s.roTxn != nil {
+		return s.roTxn.QueryWithStats(s.ctx, stmt), s.roTxn
+	} else {
+		txn := s.client.Single()
+		return txn.QueryWithStats(s.ctx, stmt), txn
+	}
 }
 
-func (s *Session) InRoTxn() bool {
-	return s.roTxn != nil
+func (s *Session) RunQuery(stmt spanner.Statement) (*spanner.RowIterator, *spanner.ReadOnlyTransaction) {
+	if s.rwTxn != nil {
+		defer func() { s.sendHeartbeat = true }()
+		return s.rwTxn.Query(s.ctx, stmt), nil
+	} else if s.roTxn != nil {
+		return s.roTxn.Query(s.ctx, stmt), s.roTxn
+	} else {
+		txn := s.client.Single()
+		return txn.Query(s.ctx, stmt), txn
+	}
+}
+
+func (s *Session) RunAnalyzeQuery(stmt spanner.Statement) (*pb.QueryPlan, error) {
+	if s.rwTxn != nil {
+		defer func() { s.sendHeartbeat = true }()
+		return s.rwTxn.AnalyzeQuery(s.ctx, stmt)
+	} else if s.roTxn != nil {
+		return s.roTxn.AnalyzeQuery(s.ctx, stmt)
+	} else {
+		txn := s.client.Single()
+		return txn.AnalyzeQuery(s.ctx, stmt)
+	}
+}
+
+func (s *Session) RunUpdate(stmt spanner.Statement) (int64, error) {
+	if s.rwTxn == nil {
+		// TODO: error
+	}
+
+	defer func() { s.sendHeartbeat = true }()
+	return s.rwTxn.Update(s.ctx, stmt)
 }
 
 func (s *Session) Close() {
@@ -156,37 +254,25 @@ func (s *Session) RecreateClient() error {
 	return nil
 }
 
-// StartHeartbeat starts heartbeat for read-write transaction.
+// startHeartbeat starts heartbeat for read-write transaction.
 //
 // If no reads or DMLs happen within 10 seconds, the rw-transaction is considered idle at Cloud Spanner server.
 // This "SELECT 1" query prevents the transaction from being considered idle.
 // cf. https://godoc.org/cloud.google.com/go/spanner#hdr-Idle_transactions
-func (s *Session) StartHeartbeat() {
+func (s *Session) startHeartbeat() {
 	interval := time.NewTicker(5 * time.Second)
-	timeout := time.NewTimer(600 * time.Second)
-	stop := make(chan bool)
-	go func() {
-		defer interval.Stop()
-		defer timeout.Stop()
+	defer interval.Stop()
 
-		for {
-			select {
-			case <-interval.C:
-				if err := heartbeat(s.ctx, s.rwTxn); err != nil {
-					return
-				}
-			case <-timeout.C:
-				return
-			case <-stop:
-				return
+	for {
+		select {
+		case <-interval.C:
+			s.rwTxnMutex.Lock()
+			if s.rwTxn != nil && s.sendHeartbeat {
+				heartbeat(s.ctx, s.rwTxn)
 			}
+			s.rwTxnMutex.Unlock()
 		}
-	}()
-	s.heartbeatChan = stop
-}
-
-func (s *Session) StopHeartbeat() {
-	close(s.heartbeatChan)
+	}
 }
 
 func heartbeat(ctx context.Context, txn *spanner.ReadWriteStmtBasedTransaction) error {
